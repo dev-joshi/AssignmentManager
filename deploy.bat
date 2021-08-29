fa9a22f6e3c3d137476e8aca1cf2a57a2bd42256
gprachi <gprachi@vmware.com>
2021-08-29 23:23:03 +0530

    nginx publisher change

--- deploy.bat
+++ deploy.bat
@@ -4,6 +4,9 @@ REM Commands to deploy Assignment Manager app
 REM set working directory to bat file parent directory
 cd /d %~dp0
 
+REM Build nginx server for proxying angular apps
+docker build --rm -t prachi-am-nginx:latest -f UI\nginx\DockerFile UI\
+
 REM Build Angular Home app
 docker build --rm -t prachi-am-home:latest -f UI\HomeApp\DockerFile UI\
 
@@ -12,9 +15,6 @@ docker build --rm -t prachi-am-publisher:latest -f UI\PublisherApp\DockerFile UI
 
 REM Build Angular Subscriber app
 docker build --rm -t prachi-am-subscriber:latest -f UI\SubscriberApp\DockerFile UI\
-
-REM Build nginx server for proxying angular apps
-docker build --rm -t prachi-am-nginx:latest -f UI\nginx\DockerFile UI\
 
 REM Spawn containers using docker compose file
 docker-compose -f docker-compose.yml up
